6e3841388d88067516092837bf3ec80759b4540b
Alan Watkins <a.watkins136@yahoo.co.uk>
2016-01-25 12:37:03 +0000

    Added switch statement with subtraction command

--- maths_functions.c
+++ maths_functions.c
@@ -3,7 +3,14 @@
 int main(int argc, char *argv[])
 {
 	char c;
-	printf("Choose a function from add (a), subtract (b), multiply(m) or divide (d)\n");
+	printf("Choose a function from add (a), subtract (s), multiply(m) or divide (d)\n");
 	scanf("%c", &c);
-	switch
+	switch (c) {
+	case a:
+		printf("%i\n", argv[1] + argv[2]);
+		break;
+	case s:
+		printf("%i\n", argv[1] - argv[2]);
+		break;
+		
 }
