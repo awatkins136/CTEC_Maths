debc87f0372e9c78ddc83c91b94f79f5c4bcbcd4
Alan Watkins <a.watkins136@yahoo.co.uk>
2016-01-25 17:15:28 +0000

    Added remainder case in maths_functions.c

--- maths_functions.c
+++ maths_functions.c
@@ -6,7 +6,7 @@ int main()
 	printf("Type two integer values:\n");
 	scanf("%i", &j);
 	scanf("%i", &k);
-	printf("Choose a function from add (1), subtract (2), multiply(3) or divide (4):\n");
+	printf("Choose a function from add (1), subtract (2), multiply(3), divide (4) or remainder (5):\n");
 	scanf("%i", &l);
 	switch (l) {
 	case 1:
@@ -23,6 +23,10 @@ int main()
 		break;
 	case 4:
 		i=j/k;
+		printf("%i\n", i);
+		break;
+	case 5:
+		i=j%k;
 		printf("%i\n", i);
 		break;
 	default:
