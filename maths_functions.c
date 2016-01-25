4f8252e1f97e2b947cea624ddc5f6bcc8badf243
Alan Watkins <a.watkins136@yahoo.co.uk>
2016-01-25 16:08:50 +0000

    Fixed integer input to maths_functions.c

--- maths_functions.c
+++ maths_functions.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[])
+int main()
 {
 	char c;
+	int i, j, k;
+	printf("Type two integer values:\n");
+	scanf("%i", &j);
+	scanf("%i", &k);
 	printf("Choose a function from add (a), subtract (s), multiply(m) or divide (d)\n");
 	scanf("%c", &c);
 	switch (c) {
-	case a:
-		printf("%i\n", argv[1] + argv[2]);
+	case 'a':
+		printf("%i\n", i);
 		break;
-	case s:
-		printf("%i\n", argv[1] - argv[2]);
+	case 's':
+		printf("%i\n", i);
 		break;
-	case m:
-		printf("%i\n", argv[1] * argv[2]);
+	case 'm':
+		printf("%i\n", i);
 		break;
-	case d:
-		printf("%i\n", argv[1] / argv[2]);
+	case 'd':
+		printf("%i\n", i);
 		break;
 	default:
 		break;
